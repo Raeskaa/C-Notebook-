724da858dc33cd56f2371dab9040ce55f0d13f29
Mahesh Tripathi <84667136+Raeskaa@users.noreply.github.com>
2021-10-24 17:41:45 +0530

    Update Codes.cpp

--- Codes.cpp
+++ Codes.cpp
@@ -1,3 +1,25 @@
+// Code to find out if a number is 
+
+int main()
+{
+     int n;
+     cin >> n;
+     
+     int total =0;
+     int originaln=n;
+     while(n>0){
+         int lastdigit = (n%10);
+         total += pow(lastdigit,3);
+         n= n/10;
+     }
+     if(total == originaln){
+         cout<< "Yes"<< endl;
+     }
+     else{
+         cout<< "fuckyou";
+     }
+}
+
 // Code to make a function which calculates factorial
 
 int Factorialfunc( int n){
