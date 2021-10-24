7b60d69da2d5d2f8f789515d23c4a7a16ed2b516
Mahesh Tripathi <84667136+Raeskaa@users.noreply.github.com>
2021-10-24 18:30:44 +0530

    Update Codes.cpp

--- Codes.cpp
+++ Codes.cpp
@@ -36,4 +36,61 @@ int main()
     cout<< Factorialfunc(a);
 }
 
+/* 
+a) Only names of variables are passed and not their types while calling a function
+b) We pass the values ,not varibles themselves. Loval variables are created in functions which are destroyed on returning from them
+c) A function can be called from any other function or main function
+*/
+// Questions on functions
+// Printing all prime numbers btween a and b
+
+int main()
+{
+    int a,b;
+    cin >> a>>b;
+    
+    for(int i=a; i<=b; i++){
+        for(int m=2; m<i; m++){
+            if(i%m==0){
+                break;
+            }
+            else{
+                if(m==i-1){
+                    cout<< i<< " ";
+                }
+                continue;
+            }
+        }
+    }
+}
+
+// Printing all prime numbers btween a and b using function ( Imma gettin error ) 
+
+bool PrimeOrNot(int n){
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            cout<< 0;
+            break;
+        }
+        else{
+            if(i==n-1){
+                cout<<1;
+            }
+            continue;
+        }
+    }
+    return 0;
+}
+int main(){
+    int a,b;
+    cin >> a>> b;
+     for( int x= a+1; x<b;x++ ){
+         if( PrimeOrNot(x)==1){
+        cout<< x;
+    }
+    else{
+        continue;
+    }
+     }
+}
 
